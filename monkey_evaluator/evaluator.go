60550a426aef1b11ab550675490ce24f996ed6e8
boyMatthew <brojour@163.com>
2025-08-15 22:34:55 +0800

    The second step of evaluator.go.

--- monkey_evaluator/evaluator.go
+++ monkey_evaluator/evaluator.go
@@ -1,6 +1,7 @@
 package monkey_evaluator
 
 import (
+	"fmt"
 	ast "myMonkey/monkey_ast"
 	object "myMonkey/monkey_object"
 )
@@ -11,31 +12,75 @@ var (
 	FALSE = &object.Boolean{Value: false}
 )
 
+func newError(format string, a ...interface{}) *object.Error {
+	return &object.Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func isError(obj object.Object) bool {
+	return obj != nil && obj.Type() == object.ERROR_OBJ
+}
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.BlockStatement:
+		return evalBlock(node)
 	case *ast.DecimalLiteral:
 		return &object.Decimal{Value: node.Value}
 	case *ast.Boolean:
 		return convertBoolean(node.Value)
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
+		if isError(right) {
+			return right
+		}
 		return evalPrefix(node.Operator, right)
 	case *ast.InfixExpression:
 		left := Eval(node.Left)
+		if isError(left) {
+			return left
+		}
 		right := Eval(node.Right)
+		if isError(right) {
+			return right
+		}
 		return evalInfix(node.Operator, left, right)
+	case *ast.ConditionExpression:
+		return evalCondition(node)
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		if isError(val) {
+			return val
+		}
+		return &object.ReturnValue{Value: val}
 	}
 	return nil
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(stmts []ast.Statement) object.Object {
 	var result object.Object
 	for _, stmt := range stmts {
 		result = Eval(stmt)
+		switch result := result.(type) {
+		case *object.ReturnValue:
+			return result.Value
+		case *object.Error:
+			return result
+		}
+	}
+	return result
+}
+
+func evalBlock(block *ast.BlockStatement) object.Object {
+	var result object.Object
+	for _, stmt := range block.Statements {
+		result = Eval(stmt)
+		if result != nil && (result.Type() == object.RETURN_VALUE_OBJ || result.Type() == object.ERROR_OBJ) {
+			return result
+		}
 	}
 	return result
 }
@@ -50,7 +95,7 @@ func convertBoolean(input bool) *object.Boolean {
 func evalPrefix(op string, right object.Object) object.Object {
 	switch op {
 	case "!":
-		return evalReverse(right)
+		return convertBoolean(!isTrue(right))
 	case "-":
 		return evalMinus(right)
 	case "++":
@@ -58,26 +103,26 @@ func evalPrefix(op string, right object.Object) object.Object {
 	case "--":
 		return evalBumpMinus(right)
 	default:
-		return NULL
+		return newError("unknown operator: %s%s", op, right.Type())
 	}
 }
 
-func evalReverse(right object.Object) object.Object {
-	switch right {
+func isTrue(obj object.Object) bool {
+	switch obj {
 	case TRUE:
-		return FALSE
+		return true
 	case FALSE:
-		return TRUE
+		return false
 	case NULL:
-		return TRUE
+		return false
 	default:
-		return FALSE
+		return true
 	}
 }
 
 func evalMinus(right object.Object) object.Object {
 	if right.Type() != object.DECIMAL_OBJ {
-		return NULL
+		return newError("unknown operator: -%s", right.Type())
 	}
 	value := right.(*object.Decimal).Value
 	return &object.Decimal{Value: -value}
@@ -85,7 +130,7 @@ func evalMinus(right object.Object) object.Object {
 
 func evalBumpPlus(right object.Object) object.Object {
 	if right.Type() != object.DECIMAL_OBJ {
-		return NULL
+		return newError("unknown operator: ++%s", right.Type())
 	}
 	value := right.(*object.Decimal).Value
 	return &object.Decimal{Value: value + 1}
@@ -93,7 +138,7 @@ func evalBumpPlus(right object.Object) object.Object {
 
 func evalBumpMinus(right object.Object) object.Object {
 	if right.Type() != object.DECIMAL_OBJ {
-		return NULL
+		return newError("unknown operator: --%s", right.Type())
 	}
 	value := right.(*object.Decimal).Value
 	return &object.Decimal{Value: value - 1}
@@ -101,6 +146,8 @@ func evalBumpMinus(right object.Object) object.Object {
 
 func evalInfix(op string, left, right object.Object) object.Object {
 	switch {
+	case left.Type() != right.Type():
+		return newError("type mismatch: %s %s %s", left.Type(), op, right.Type())
 	case left.Type() == object.DECIMAL_OBJ && right.Type() == object.DECIMAL_OBJ:
 		return evalDecimalInfix(op, left.(*object.Decimal).Value, right.(*object.Decimal).Value)
 	case op == "==":
@@ -108,7 +155,7 @@ func evalInfix(op string, left, right object.Object) object.Object {
 	case op == "!=":
 		return convertBoolean(left != right)
 	default:
-		return NULL
+		return newError("unknown operator: %s %s %s", left.Type(), op, right.Type())
 	}
 }
 
@@ -139,6 +186,19 @@ func evalDecimalInfix(op string, left, right float64) object.Object {
 	case "<=":
 		return convertBoolean(left <= right)
 	default:
+		return newError("unknown operator: %f %s %f", left, op, right)
+	}
+}
+
+func evalCondition(ce *ast.ConditionExpression) object.Object {
+	condition := Eval(ce.Condition)
+	if isError(condition) {
+		return condition
+	} else if isTrue(condition) {
+		return Eval(ce.True)
+	} else if ce.False != nil {
+		return Eval(ce.False)
+	} else {
 		return NULL
 	}
 }
--- monkey_evaluator/evaluator_test.go
+++ monkey_evaluator/evaluator_test.go
@@ -48,18 +48,6 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
-	}
-	for _, test := range tests {
-		evaluated := testEval(test.input)
-		testBooleanObj(t, evaluated, test.expected)
-	}
-}
-
-func TestReverse(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected bool
-	}{
 		{"!true", false},
 		{"!false", true},
 		{"!5", false},
@@ -80,6 +68,48 @@ func TestReverse(t *testing.T) {
 	for _, test := range tests {
 		evaluated := testEval(test.input)
 		testBooleanObj(t, evaluated, test.expected)
+	}
+}
+
+func TestConditionExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if(true){10}", 10.0},
+		{"if(false){10}", nil},
+		{"if(1){10}", 10.0},
+		{"if(1<2){10}", 10.0},
+		{"if(1>2){10}", nil},
+		{"if(1<2){10}else{20}", 10.0},
+		{"if(1>2){10}else{20}", 20.0},
+	}
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		decimal, ok := test.expected.(float64)
+		if ok {
+			testDecimalObj(t, evaluated, decimal)
+		} else {
+			testNullObj(t, evaluated)
+		}
+	}
+}
+
+func TestReturnValue(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected float64
+	}{
+		{"ret 10;", 10.0},
+		{"ret 10;9;", 10.0},
+		{"ret 2*5;9;", 10.0},
+		{"9;ret 2*5;9;", 10.0},
+		{"if(10>1){if(10>1){ret 10;}ret 1;}", 10.0},
+	}
+
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		testDecimalObj(t, evaluated, test.expected)
 	}
 }
 
@@ -111,6 +141,14 @@ func testBooleanObj(t *testing.T, obj object.Object, expected bool) bool {
 	}
 	if result.Value != expected {
 		t.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func testNullObj(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("object is not NULL. got=%T (%+v)", obj, obj)
 		return false
 	}
 	return true
